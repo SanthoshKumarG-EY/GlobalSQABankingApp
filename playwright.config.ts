af795d37b2a13c6d75775c6bbbb0814572b2f529
SanthoshKumarG-EY <santhosh.kumar.g@gds.ey.com>
2026-04-08 10:33:54 +0530

    Updated config to pass in github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-   headless:false,
+   headless:true,
    viewport:{width:1280,height:720},
   },
 
