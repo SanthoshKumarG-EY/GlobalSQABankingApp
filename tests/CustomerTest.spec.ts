66d9644c1df7dada3f08d86864dcdaca3a6d67ee
SanthoshKumarG-EY <santhosh.kumar.g@gds.ey.com>
2026-04-08 11:20:47 +0530

    Updated customer test

--- tests/CustomerTest.spec.ts
+++ tests/CustomerTest.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from "../src/fixtures/BaseFixture";
-import { TestDataManager } from "../src/utils/TestManager";
-import { DataGenerator } from "../src/utils/DataGenerator";
+import customerTestData from '../testdata/customerTest.json';
+
+const { customer } = customerTestData;
 
 test.describe('Customer Tests', () => {
-    test('Customer Login', async ({ homePage, customerPage }) => {
-        const customerData = TestDataManager.readData('customerData');  
+    test('Customer Login', async ({ homePage, customerPage }) => { 
         await homePage.navigateTo('/angularJs-protractor/BankingProject/#/login');
         await homePage.clickCustomerLogin();
-        await customerPage.login('Harry Potter');
+        await customerPage.login(customer.name);
         await expect(customerPage.accountNumber).toBeVisible();
-        await customerPage.selectAccount('1004');
+        await customerPage.selectAccount(customer.accountNumber);
         const accountNumber = await customerPage.getText(customerPage.accountNumber);
         const accountBalance = await customerPage.getText(customerPage.accountBalance);
         const accountCurrency = await customerPage.getText(customerPage.accountCurrency);
@@ -17,28 +17,24 @@ test.describe('Customer Tests', () => {
     });
 
     test('Deposit Money', async ({ homePage, customerPage }) => {               
-        const customerData = TestDataManager.readData('customerData');
-        const accountData = TestDataManager.readData('accountData');
         await homePage.navigateTo('/angularJs-protractor/BankingProject/#/login');
         await homePage.clickCustomerLogin();
-        await customerPage.login('Harry Potter');
+        await customerPage.login(customer.name);
         await expect(customerPage.accountNumber).toBeVisible();
-        await customerPage.selectAccount('1004');
-        await customerPage.deposit('500');
+        await customerPage.selectAccount(customer.accountNumber);
+        await customerPage.deposit(customer.amount);
         const depositMessage = await customerPage.getMessage();
         console.log("Deposit Message: " + depositMessage);
         expect(depositMessage).toBe('Deposit Successful');
     });
 
     test('Withdraw Money', async ({ homePage, customerPage }) => {               
-        const customerData = TestDataManager.readData('customerData');
-        const accountData = TestDataManager.readData('accountData');
         await homePage.navigateTo('/angularJs-protractor/BankingProject/#/login');
         await homePage.clickCustomerLogin();
-        await customerPage.login('Harry Potter');
+        await customerPage.login(customer.name);
         await expect(customerPage.accountNumber).toBeVisible();
-        await customerPage.selectAccount('1004');
-        await customerPage.withdraw('100'); 
+        await customerPage.selectAccount(customer.accountNumber);
+        await customerPage.withdraw(customer.amount); 
         const withdrawMessage = await customerPage.getMessage();
         console.log("Withdraw Message: " + withdrawMessage);
         expect(withdrawMessage).toBe('Transaction Failed. You can not withdraw amount more than the balance.');
