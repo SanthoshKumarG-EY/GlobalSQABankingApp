11a3d8ad4e7d381c6d25dee12f2adf9341989beb
SanthoshKumarG-EY <santhosh.kumar.g@gds.ey.com>
2026-04-20 10:00:31 +0530

    Updated tsconfig

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,10 +20,10 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], ['list']],
-  globalTimeout:20000,
+  globalTimeout:30000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'https://www.globalsqa.com/',
